05841364d44d7bbcbff980c720a1500ca4928944
JuanpeTDB <albanelljuanpedro@gmail.com>
2025-11-12 13:42:49 -0300

    Ej 8 y Ej 9

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,14 +5,60 @@
 
 using namespace std;
 
+int max(int a, int b){
+    return a > b ? a : b;
+    
+}
+
+int func(int* numeros, int*** memo, int i, int f, int k){
+    if (i == f) return ((k+1)*(k+1));
+    if (i > f) return 0;
+    if (memo[i][f][k] != -1) return memo[i][f][k];
+    
+
+    int max1 = 0;
+    int aux1 = 0;
+    int aux2 = 0;
+
+    for (int m = i+1; m <= f; m++)
+    {
+        if (numeros[m] == numeros[i])
+        {
+            aux1 = func(numeros, memo, i+1, m-1, 0);
+            memo[i+1][m-1][0] = aux1;
+            aux2 = func(numeros, memo, m, f, k+1);
+            memo[m][f][k+1] = aux2;
+            if (max1 < aux1 + aux2) max1 = aux1 + aux2;
+        }
+    }
+
+    int max2 = func(numeros, memo, i+1, f, 0) + ((k+1)*(k+1));
+
+    int res = max(max1, max2);
+    memo[i][f][k] = res;
+    return res;
+}
+
 int main()
 {
     // TODO
     int N;
     cin >> N;
-    int* numeros = new int[N];
+    int *numeros = new int[N];
     for (int i = 0; i < N; i++) cin >> numeros[i];
-    
 
+    int ***memo = new int **[N];
+    for (int i = 0; i < N; i++){
+        memo[i] = new int*[N];
+        for (int f = 0; f < N; f++){
+            memo[i][f] = new int[N];
+            for (int k = 0; k < N; k++){
+                 memo[i][f][k] = -1;
+            }
+        }
+    }
+
+    int res = func(numeros, memo, 0, N-1, 0);
+    cout << res << endl;
     return 0;
 }
--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,8 +5,73 @@
 
 using namespace std;
 
+int max(int a, int b){
+    return a > b ? a : b;
+    
+}
+
 int main()
 {
     // TODO
+    int N;
+    cin >> N;
+    int S;
+    cin >> S;
+    int L;
+    cin >> L;
+    int* tams = new int[N+1];
+    int* lins = new int[N+1];
+    int* pts = new int[N+1];
+
+    for (int i = 0; i <= N; i++)
+    {
+        cin >> tams[i];
+        cin >> lins[i];
+        cin >> pts[i];
+    }
+
+    int ***tab = new int **[N+1];
+    for (int i = 0; i <= N; i++){
+        tab[i] = new int*[S+1];
+        for (int f = 0; f <= S; f++){
+            tab[i][f] = new int[L+1];
+            for (int k = 0; k <= L; k++){
+                 tab[i][f][k] = 0;
+            }
+        }
+    }
+
+    int maxP = 0;
+
+    for (int i = 1; i <= N; i++)
+    {
+        int auxT = tams[i];
+        int auxL = lins[i];
+        int auxPts = pts[i];
+        for (int j = 0; j <= S; j++)
+        {
+            for (int k = 0; k <= L; k++)
+            {
+                if (auxT > j || auxL > k)
+                {
+                    tab[i][j][k] = tab[i-1][j][k];
+                }
+                else
+                {
+                    int valorTomar = auxPts + tab[i-1][j - auxT][k - auxL];
+                    int valorNoTomar = tab[i-1][j][k];
+                    int conviene = max(valorTomar, valorNoTomar);
+                    tab[i][j][k] = conviene;
+                    if (conviene > maxP) maxP = conviene;
+                }
+                
+            }
+            
+        }
+     
+    }
+
+    cout << maxP << endl;
+
     return 0;
 }
