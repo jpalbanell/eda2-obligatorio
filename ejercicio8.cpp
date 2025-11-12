7a135b092df7635fa0900d62300af84968b8d89a
JuanpeTDB <albanelljuanpedro@gmail.com>
2025-11-12 09:20:31 -0300

    Actualizacion

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -8,5 +8,11 @@ using namespace std;
 int main()
 {
     // TODO
+    int N;
+    cin >> N;
+    int* numeros = new int[N];
+    for (int i = 0; i < N; i++) cin >> numeros[i];
+    
+
     return 0;
 }
